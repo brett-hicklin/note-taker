c5a29893de4d4b30bf4d33dbc5a555e17d7e6f72
Brett Hicklin <hicklin.brett@gmail.com>
2023-03-18 11:36:20 -0400

    created route for * to return the index.html file

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,9 @@ app.get('/notes', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
